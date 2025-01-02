32d32d4142cbf6b56ca624c5c9dd98df2cb392d5
DTMVithana <168225961+DTMVithana@users.noreply.github.com>
2025-01-02 22:37:18 +0530

    Add tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all React components
+  ],
+  theme: {
+    extend: {}, // Customize the default theme if needed
+  },
+  plugins: [], // Add any Tailwind plugins here if needed
+};
